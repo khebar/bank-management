eae2a682066e17a2452eeac8ad8a784eb242f37b
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-07-19 20:26:12 +0330

    اضافه کردن متد های کانستراکتور و دیستراکتور و add و remove

--- linkedlist.h
+++ linkedlist.h
@@ -22,3 +22,54 @@ public:
     void add(T value);
     bool remove(int index);
 };
+
+template <typename T>
+LinkedList<T>::LinkedList() : head(nullptr), size(0) {}
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    Node<T>* current = head;
+    while (current != nullptr) {
+        Node<T>* nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+    head = nullptr;
+    size = 0;
+}
+template <typename T>
+void LinkedList<T>::add(T value) {
+    Node<T>* newNode = new Node<T>(value);
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node<T>* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    size++;
+}
+
+template <typename T>
+bool LinkedList<T>::remove(int index) {
+    if (index < 0 || index >= size || head == nullptr) {
+        return false; 
+    }
+    Node<T>* toDelete;
+    if (index == 0) {
+        toDelete = head;
+        head = head->next;
+    } else {
+        Node<T>* current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current->next;
+        }
+        toDelete = current->next;
+        current->next = toDelete->next;
+    }
+    
+    delete toDelete;
+    size--;
+    return true;
+}
