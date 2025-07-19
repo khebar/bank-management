6fcd9e0d9e25d37ee9436bcfa08002368ac55cf3
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-07-19 20:50:05 +0330

    حذف User.cpp از برنچ User-h

--- User.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "User.h"
-
-User::User(const std::string& first, const std::string& last, 
-           const std::string& national, int userAge,
-           const std::string& user, const std::string& pass)
-    : firstName(first), lastName(last), nationalCode(national),
-      age(userAge), username(user), password(pass) {
-}
