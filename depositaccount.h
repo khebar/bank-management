586d4b62779660c2b5a635e3d20b6566b3b7fc76
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-07-21 06:03:11 +0330

    اضافه کردن متد های حساب سپرده

--- depositaccount.h
+++ depositaccount.h
@@ -9,4 +9,7 @@ public:
     DepositAccount(const string& cardNum, const string& accNum, const string& ibanNum, double initialBalance, double rate, int termMonths);
     string getAccountType() const override;
     double getInterestRate() const override;
+    double calculateInterest(int months) const;
+    int getRemainingTerm() const;
+    void extendTerm(int additionalMonths);
 };
