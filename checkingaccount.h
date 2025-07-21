75bc1101af35915abf2453d3b156a6e996ae4b45
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-07-21 06:10:58 +0330

    تعریف

--- checkingaccount.h
+++ checkingaccount.h
@@ -1,21 +1,16 @@
-#ifndef CURRENTACCOUNT_H
-#define CURRENTACCOUNT_H
-
 #include "account.h"
 #include <string>
 using namespace std;
-class CurrentAccount : public Account {
+class CheckingAccount : public Account {
 private:
     double overdraftLimit;  // سقف اضافه برداشت
     double overdraftInterest; // نرخ بهره اضافه برداشت
 
 public:
-    CurrentAccount(const string& cardNum, const string& accNum, 
+    CheckingAccount(const string& cardNum, const string& accNum, 
                    const string& ibanNum, double initialBalance,
                    double overLimit = 0.0, double overInterest = 0.0);
     
     string getAccountType() const override;
     double getInterestRate() const override;
 };
-
-#endif // CURRENTACCOUNT_H
