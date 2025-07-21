90332aa4cbfd5af55803f3f5c5637c4e2df4cabb
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-07-21 20:45:46 +0330

    farsi

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <QApplication>
 #include <QLabel>
+#include <QLocale>
+#include <QTranslator>
+#include <QFontDatabase>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QLabel label("سیستم مدیریت بانکداری");
     label.show();
+    return a.exec();
+    //
+    QFontDatabase::addApplicationFont(":/fonts/Vazir.ttf");
+    QFont font("Vazir", 10);
+    a.setFont(font);
+
+    a.setLayoutDirection(Qt::RightToLeft);
+
+    MainWindow w;
+    w.show();
     return a.exec();
 }
